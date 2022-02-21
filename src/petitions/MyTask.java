f6f48046617330ef00b0a2c4a14109352912ad59
B1SK70 <78968157+B1SK70@users.noreply.github.com>
2022-02-21 17:59:25 +0100

    test

--- src/petitions/MyTask.java
+++ src/petitions/MyTask.java
@@ -9,7 +9,7 @@ public class MyTask {
         int port = 10001;
 
         Petitions p1 = new Petitions("Arnau", port, "localhost");
-        new Thread(p1).start();
+          new Thread(p1).start();
         
         Petitions p2 = new Petitions("Jandro", port);
         new Thread(p2).start();
